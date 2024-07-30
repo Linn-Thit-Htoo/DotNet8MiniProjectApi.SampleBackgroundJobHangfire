d7352d5ed951ed1aa60bc317ddad7fe739a3b5fc
Linn Thit Htoo <lth1212001@gmail.com>
2024-07-30 13:15:25 +0630

    Format Code

--- DotNet8MiniProjectApi.SampleBackgroundJobHangfire/DependencyInjection.cs
+++ DotNet8MiniProjectApi.SampleBackgroundJobHangfire/DependencyInjection.cs
@@ -1,52 +1,47 @@
-﻿using DotNet8MiniProjectApi.SampleBackgroundJobHangfire.Features.Setup;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
+﻿namespace DotNet8MiniProjectApi.SampleBackgroundJobHangfire;
 
-namespace DotNet8MiniProjectApi.SampleBackgroundJobHangfire
+public static class DependencyInjection
 {
-    public static class DependencyInjection
+    public static IServiceCollection AddDependencyInjection(this IServiceCollection services, WebApplicationBuilder builder)
     {
-        public static IServiceCollection AddDependencyInjection(this IServiceCollection services, WebApplicationBuilder builder)
-        {
-            return services.AddDbContextService(builder)
-                .AddDataAccessService()
-                .AddBusinessLogicService()
-                .AddHangfireService(builder);
-        }
+        return services.AddDbContextService(builder)
+            .AddDataAccessService()
+            .AddBusinessLogicService()
+            .AddHangfireService(builder);
+    }
 
-        private static IServiceCollection AddDbContextService(this IServiceCollection services, WebApplicationBuilder builder)
+    private static IServiceCollection AddDbContextService(this IServiceCollection services, WebApplicationBuilder builder)
+    {
+        return builder.Services.AddDbContext<AppDbContext>(opt =>
         {
-            return builder.Services.AddDbContext<AppDbContext>(opt =>
-            {
-                opt.UseSqlServer(builder.Configuration.GetConnectionString("DbConnection"));
-            }, ServiceLifetime.Transient);
-        }
+            opt.UseSqlServer(builder.Configuration.GetConnectionString("DbConnection"));
+        }, ServiceLifetime.Transient);
+    }
 
-        private static IServiceCollection AddDataAccessService(this IServiceCollection services)
+    private static IServiceCollection AddDataAccessService(this IServiceCollection services)
+    {
+        return services.AddScoped<DL_Setup>();
+    }
+
+    private static IServiceCollection AddBusinessLogicService(this IServiceCollection services)
+    {
+        return services.AddScoped<BL_Setup>();
+    }
+
+    private static IServiceCollection AddHangfireService(
+        this IServiceCollection services,
+        WebApplicationBuilder builder
+    )
+    {
+        builder.Services.AddHangfire(opt =>
         {
-            return services.AddScoped<DL_Setup>();
-        }
+            opt.UseSqlServerStorage(builder.Configuration.GetConnectionString("DbConnection"))
+                .SetDataCompatibilityLevel(CompatibilityLevel.Version_180)
+                .UseSimpleAssemblyNameTypeSerializer()
+                .UseRecommendedSerializerSettings();
+        });
 
-        private static IServiceCollection AddBusinessLogicService(this IServiceCollection services)
-        {
-            return services.AddScoped<BL_Setup>();
-        }
-
-        private static IServiceCollection AddHangfireService(
-            this IServiceCollection services,
-            WebApplicationBuilder builder
-        )
-        {
-            builder.Services.AddHangfire(opt =>
-            {
-                opt.UseSqlServerStorage(builder.Configuration.GetConnectionString("DbConnection"))
-                    .SetDataCompatibilityLevel(CompatibilityLevel.Version_180)
-                    .UseSimpleAssemblyNameTypeSerializer()
-                    .UseRecommendedSerializerSettings();
-            });
-
-            builder.Services.AddHangfireServer();
-            return services;
-        }
+        builder.Services.AddHangfireServer();
+        return services;
     }
 }
--- DotNet8MiniProjectApi.SampleBackgroundJobHangfire/GlobalUsings.cs
+++ DotNet8MiniProjectApi.SampleBackgroundJobHangfire/GlobalUsings.cs
@@ -1,7 +1,9 @@
 // Global using directives
 
 global using DotNet8MiniProjectApi.SampleBackgroundJobHangfire.Db.AppDbContexts;
+global using DotNet8MiniProjectApi.SampleBackgroundJobHangfire.Features.Setup;
 global using DotNet8MiniProjectApi.SampleBackgroundJobHangfire.Models;
 global using DotNet8MiniProjectApi.SampleBackgroundJobHangfire.Models.Enums;
 global using DotNet8MiniProjectApi.SampleBackgroundJobHangfire.Models.Setup;
 global using Hangfire;
+global using Microsoft.EntityFrameworkCore;
